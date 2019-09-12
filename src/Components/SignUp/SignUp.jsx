779f6c42ca8f7e6ceb4a608e2f11d7d4dcddf645
samhine <hines@elslabm-156920.science.regn.net>
2019-09-12 14:34:03 +0100

    SH: Signup guide

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { useState } from "react";
+import DatePicker from 'react-datepicker';
+
+import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 const SignUp = () => {
-    return (
-        <p>Sign Up form here</p>
-    );
-}
+  // Can now use startDate as a the DOB variable
+  const [startDate, setStartDate] = useState(new Date());
+
+  return (
+    <div className="container">
+      <img
+        width="100%"
+        src={require("../../images/Locations.jpg")}
+        alt="QA Cinemas"
+      />
+
+      <form class="form-horizontal">
+        <div class="form-group">
+          <label class="col-sm-2">Title:</label>
+          <div class="col-sm-10">
+            <select class="form-control">
+              <option>Mr.</option>
+              <option>Mrs.</option>
+              <option>Miss.</option>
+              <option>Ms.</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="col-sm-2">First Name:</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" placeholder="Enter first name"/>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="col-sm-10">Last Name:</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" placeholder="Enter last name"/>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="col-sm-2 control-label">Email</label>
+          <div class="col-sm-10">
+            <input type="email" class="form-control" placeholder="Email"/>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="col-sm-2">Date of Birth</label>
+          <div class="col-sm-10">
+            <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="col-sm-2">Phone number:</label>
+          <div class="col-sm-10">
+            <input type="number" class="form-control" placeholder="Enter email"/>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-primary">Sign up</button>
+          </div>
+        </div>
+      </form>
+
+    </div>
+  );
+};
 
 export default SignUp;
