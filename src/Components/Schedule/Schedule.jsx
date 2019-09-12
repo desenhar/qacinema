0c926d7acce881fb8fbb5c6f0c4358851896ba3e
Hilgenberg-Bastos <hilgenberg-bastosr@elslabm-156785.science.regn.net>
2019-09-12 16:29:21 +0100

    RH,importdata

--- src/Components/Schedule/Schedule.jsx
+++ src/Components/Schedule/Schedule.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import mockdata from '../../data/cinemamockdata';
 
 const Schedule = () => {
+    const listings = mockdata["openingTimes"].map(
+        day => <tr>
+            <td>{day["day"]}</td>
+            <td>{day["opening"]}</td>
+            <td>{day["close"]}</td>
+        </tr>
+    )
+
     return (
         <body>
             <div className="container">
@@ -12,22 +21,16 @@ const Schedule = () => {
             <br></br>
             <div class="container">
                 <table class="table table-bordered">
-                    <tr>
-                        <th>Days</th>
-                        <th>Opening hours</th>
-                    </tr>
-                    <tr>
-                        <td>Monday - Thursday</td>
-                        <td>11:00 - 00:00</td>
-                    </tr>
-                    <tr>
-                        <td>Friday - Saturday</td>
-                        <td>11:00 - 02:00</td>
-                    </tr>
-                    <tr>
-                        <td>Sunday</td>
-                        <td>11:00 - 23:00</td>
-                    </tr>
+                    <th>
+                        Days
+                    </th>
+                    <th>
+                        Open
+                    </th>
+                    <th>
+                        Close
+                    </th>
+                    {listings}
                 </table>
             </div>
         </body>
