4462d5e54e71ed7c74997e8f4bacbf44444535f7
karamkapoor <k.kapoor@elsevier.com>
2019-09-12 14:08:44 +0100

    KK testcode2

---
+++ src/Components/Schedule/TestCode.jsx
@@ -0,0 +1,26 @@
+< !DOCTYPE html >
+    <html lang="en">
+        <body>
+            <h1>Opening hours</h1>
+            <div class="container">
+                <table>
+                    <tr>
+                        <th>Days</th>
+                        <th>Opening hours</th>
+                    </tr>
+                    <tr>
+                        <td>Monday - Thursday</td>
+                        <td>11:00 - 00:00</td>
+                    </tr>
+                    <tr>
+                        <td>Friday - Saturday</td>
+                        <td>11:00 - 02:00</td>
+                    </tr>
+                    <tr>
+                        <td>Sunday</td>
+                        <td>11:00 - 23:00</td>
+                    </tr>
+                </table>
+            </div>
+        </body>
+    </html>
