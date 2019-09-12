a4a4a0fa6ce05223879946742255413dd6a6cf83
Mayenin <mayenino@elslabm-156923.science.regn.net>
2019-09-12 15:02:00 +0100

    OM:Replace hard coded paths with imports.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,58 @@
 import React from 'react';
+import { CONTENTARTICLETEXT } from '../../js/constants/contentArticleText';
+import { HOMEARTICLEINFO } from '../../js/constants/contentArticleText';
+import { HOMEIMAGES } from '../../js/constants/homeImage';
 
 const Home = () => {
     return (
-        <p>Home content here</p>
+        <div className="container">
+            <img width="100%" src={require('../../'+HOMEIMAGES[0].src)} alt={HOMEIMAGES[0].alt}/>
+
+            <div className="card">
+                <div className="card-body"> 
+                    <h4 class="card-title text-center">Welcome to QA Cinemas</h4>
+                    <h6 class="card-subtitle mb-2 text-muted text-center">Bringing the biggest blockbusters to you.</h6>
+                    <hr></hr>
+                    <br></br>
+                    <p class="mb-2 text-muted">
+                        {HOMEARTICLEINFO[0].title}
+                        </p>
+                        {CONTENTARTICLETEXT}
+                        {/* QA Cinemas was founded by the SquadJS in 1956 and inaugarated by The Queen in the same year.
+                    In the decades since, we have been committed to delivering the ultimate cinema experience by bringing you 
+                    the latest blockbusters. QA cinemas exude style and glamour just as the founding fathers had envisioned.
+                    Back then, in the days of post-war austerity, cinemas were gloomy, dull and drab establishments. QA Cinemas changed all that with 
+                    its first flagship branch at Tower Bridge. */}
+                    <br></br>
+                    <hr></hr>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm">
+                    <img width="100%" src={require('../../'+HOMEIMAGES[1].src)} alt={HOMEIMAGES[1].alt}/>
+                </div>
+                <div class="col-sm">
+                    <img width="100%" src={require('../../'+HOMEIMAGES[2].src)} alt={HOMEIMAGES[2].alt}/>
+                </div>
+                <div class="col-sm">
+                    <img width="100%" src={require('../../'+HOMEIMAGES[1].src)} alt={HOMEIMAGES[1].alt}/>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="card-body"> 
+                    <p class="mb-2 text-muted">
+                        <br></br>
+                        {HOMEARTICLEINFO[1].title}
+                    </p>
+                    <p>            
+                    </p>
+                    {CONTENTARTICLETEXT}
+ 
+                </div>
+            </div>
+        </div>
     );
 }
 
