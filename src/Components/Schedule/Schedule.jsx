701c5fc95eeaabfba8059d3210093d36bad5a7db
Hilgenberg-Bastos <hilgenberg-bastosr@elslabm-156785.science.regn.net>
2019-09-12 14:21:13 +0100

    RH:Schedule

--- src/Components/Schedule/Schedule.jsx
+++ src/Components/Schedule/Schedule.jsx
@@ -2,7 +2,33 @@ import React from 'react';
 
 const Schedule = () => {
     return (
-        <p>Schedule content here</p>
+        <body>
+            <div className="container">
+                <img width="100%" src={require(
+                    "../../images/popcorn.jpeg")} alt="QA Cinemas" />
+            </div>
+            <h1>Opening hours</h1>
+            <div class="container">
+                <table>
+                    <tr>
+                        <th>Days</th>
+                        <th>Opening hours</th>
+                    </tr>
+                    <tr>
+                        <td>Monday - Thursday</td>
+                        <td>11:00 - 00:00</td>
+                    </tr>
+                    <tr>
+                        <td>Friday - Saturday</td>
+                        <td>11:00 - 02:00</td>
+                    </tr>
+                    <tr>
+                        <td>Sunday</td>
+                        <td>11:00 - 23:00</td>
+                    </tr>
+                </table>
+            </div>
+        </body>
     );
 }
 
