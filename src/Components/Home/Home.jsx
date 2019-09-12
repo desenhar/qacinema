ea1b8c3b096786b9b0cba2a259393b39094e0781
Ohie Mayenin <32964045+OhieMayenin@users.noreply.github.com>
2019-09-12 13:35:36 +0100

    Homepage added banner photo, first paragraph and grid of photos.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,24 +3,32 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="container">
-            <img src={require("/Users/mayenino/Downloads/QA Files/ReactHackathonFiles/reacthackathon/src/images/Locations.jpg")} alt="QA Cinemas"/>
+            <img width="100%" src={require(
+                "../../images/Locations.jpg")} alt="QA Cinemas"/>
+
+            <div className="card">
+                <div className="card-body text-center"> 
+                    <h4 class="card-title text-center">Welcome</h4>
+                    <h6 class="card-subtitle mb-2 text-muted text-center">Welcome to QA Cinemas.</h6>
+                    <p class="mb-2 text-muted text-center">Bringing the biggest blockbusters to you.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm">
+                    <img width="100%" src={require(
+                    "../../images/InternationalHouse.jpeg")} alt="QA Cinemas"/>
+                </div>
+                <div class="col-sm">
+                    <img width="100%" src={require(
+                    "../../images/InternationalHouse2.jpeg")} alt="QA Cinemas"/>
+                </div>
+                <div class="col-sm">
+                    <img width="100%" src={require(
+                    "../../images/InternationalHouse.jpeg")} alt="QA Cinemas"/>
+                </div>
+            </div>
         </div>
-
-
-
-/* <div class="container">
-  <div class="row">
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-  </div>
-</div> */
     );
 }
 
